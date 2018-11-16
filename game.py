db4a758943ea21faf90dd3ec0aeb9b3a15e4918d
OscarGlo <oscarglo@free.fr>
2018-11-17 00:36:39 +0100

    Particle, ParticleGen and CyclePos

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 import pygame
 
@@ -51,30 +52,11 @@ class Events:
 #         soften[:, :-1] += arr[:, 1:] * factor
 #         pygame.surfarray.blit_array(surf, arr)
 
-
-class Ship:
-    color = (255, 255, 255)
-    rate = 0.0175
-
+class CyclePos:
     def __init__(self, pos):
         self.pos = pos
-        self.angle = -math.pi/2
-        self.speed = [0.0, 0.0]
-        self.ang_speed = self.for_speed = 0
 
-    def update(self):
-        self.speed[0] = self.speed[0] * (1 - Ship.rate) + self.for_speed * math.cos(self.angle) * Ship.rate
-        self.speed[1] = self.speed[1] * (1 - Ship.rate) + self.for_speed * math.sin(self.angle) * Ship.rate
-
-        self.pos[0] += self.speed[0]
-        self.pos[1] += self.speed[1]
-
-        self.angle += self.ang_speed
-
-        self.for_speed = 0
-        self.ang_speed = 0
-
-        # Cycle position
+    def cycle(self):
         if self.pos[0] >= width:
             self.pos[0] -= width
         elif self.pos[0] <= 0:
@@ -85,18 +67,116 @@ class Ship:
         elif self.pos[1] <= 0:
             self.pos[1] += height
 
+
+class Particle(CyclePos):
+    def __init__(self, pos, angle, speed, time, fade=True):
+        super().__init__(pos)
+        self.angle = angle
+        self.speed = speed
+        self.time = self.startTime = time
+        self.fade = fade
+
+        self.dead = False
+
+    def update(self):
+        self.pos[0] += self.speed * math.cos(self.angle)
+        self.pos[1] += self.speed * math.sin(self.angle)
+
+        if self.time <= 0:
+            self.dead = True
+        self.time -= 1
+
+        self.speed *= 0.98
+
+        self.cycle()
+
+    def draw(self, surf):
+        if self.fade:
+            c = 255 * (self.time / self.startTime)
+        else:
+            c = 255
+        pygame.draw.rect(surf, (c, c, c), pygame.rect.Rect(self.pos, (2, 2)))
+
+
+class ParticleGen(CyclePos):
+    def __init__(self, pos, direction, angle, delay, speed, time):
+        super().__init__(pos)
+        self.direction = direction
+        self.angle = angle
+        self.delay = delay
+        self.counter = 0
+        self.speed = speed
+        self.time = time
+
+        self.particles = []
+
+    def generate(self, speed_var=0.2, time_var=0.1, fade=True):
+        if self.counter == self.delay:
+            self.counter = 0
+            angle = self.direction + (random.random() * self.angle - self.angle / 2)
+            speed = self.speed + (self.speed * random.random() - self.speed / 2) * speed_var
+            time = self.time + (self.time * random.random() - self.time / 2) * time_var
+            self.particles.append(Particle(self.pos, angle, speed, self.time, fade))
+        else:
+            self.counter += 1
+
+        self.cycle()
+
+    def update(self):
+        for p in self.particles:
+            p.update()
+            if p.dead:
+                self.particles.remove(p)
+
+    def draw(self, surf):
+        for particle in self.particles:
+            particle.draw(surf)
+
+
+class Ship:
+    color = (255, 255, 255)
+    rate = 0.0175
+
+    def __init__(self, pos):
+        self.pos = pos
+        self.angle = -math.pi / 2
+        self.speed = [0.0, 0.0]
+        self.ang_speed = self.for_speed = 0
+
+        self.particles = ParticleGen(rotate_point(self.pos, [-10, 0], self.angle),
+                                     self.angle + math.pi, math.pi / 2, 5, 2, 60)
+
+    def update(self):
+        self.speed[0] = self.speed[0] * (1 - Ship.rate) + self.for_speed * math.cos(self.angle) * Ship.rate
+        self.speed[1] = self.speed[1] * (1 - Ship.rate) + self.for_speed * math.sin(self.angle) * Ship.rate
+
+        self.pos[0] += self.speed[0]
+        self.pos[1] += self.speed[1]
+
+        self.angle += self.ang_speed
+
+        self.particles.pos = rotate_point(self.pos, [-10, 0], self.angle)
+        self.particles.direction = self.angle + math.pi
+        self.particles.update()
+        if self.for_speed != 0:
+            self.particles.generate()
+
+        self.for_speed = 0
+        self.ang_speed = 0
+
     def draw_one(self, surf, pos):
         pygame.draw.lines(surf, Ship.color, True, [
-                                rotate_point(pos, [15, 0], self.angle),
-                                rotate_point(pos, [-15, -15], self.angle),
-                                rotate_point(pos, [-10, 0], self.angle),
-                                rotate_point(pos, [-15, 15], self.angle)
-                            ], 2)
+            rotate_point(pos, [15, 0], self.angle),
+            rotate_point(pos, [-15, -15], self.angle),
+            rotate_point(pos, [-10, 0], self.angle),
+            rotate_point(pos, [-15, 15], self.angle)
+        ], 2)
 
     def draw(self, surf):
         for i in (-1, 0, 1):
             for j in (-1, 0, 1):
                 self.draw_one(surf, (self.pos[0] + width * i, self.pos[1] + height * j))
+        self.particles.draw(surf)
 
 
 class Game:
